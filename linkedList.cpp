a83fb671e4075193fec576eb3ce956c649106a41
chasem <chasem@oregonstate.edu>
2023-05-01 23:44:32 -0700

    started a struct

--- linkedList.cpp
+++ linkedList.cpp
@@ -6,3 +6,15 @@ using namespace std;
 struct Node {
 
 };
+
+
+
+// function that inserts given value to given position
+
+
+// function that removes value from given position and returns it
+
+
+// function that takes a position and returns that value without removing it
+
+// te
--- linkedList.h
+++ linkedList.h
@@ -1,9 +1,17 @@
 #ifndef CS260_SPRING_23_ASSIGNMENT_4_LINKEDLIST_H
 #define CS260_SPRING_23_ASSIGNMENT_4_LINKEDLIST_H
 
+struct Node {
+
+};
+
 
 class linkedList {
+    public:
 
+    void add(int someValue, int somePosition){
+
+    }
 };
 
 
