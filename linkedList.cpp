c1006497e9cbbb56a0f67d3610ee427415ffa030
chasem <chasem@oregonstate.edu>
2023-05-10 14:25:21 -0700

    added while loop to reach position in list.

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,8 +1,45 @@
-#include "linkedList.h"
+//#include "linkedList.h"
 #include <iostream>
 
 using namespace std;
 
+struct Node {
+    int data;
+    node* next;
+};
+
+
+class linkedList {
+private:
+    node* head;
+
+public:
+    linkedList() {
+        head = nullptr; // allows me to handle initial edge case
+    }
+
+    void add(int someValue, int somePosition){
+        node *new_node = new node;
+        new_node->data = someValue;
+        new_node->next = nullptr;
+
+        node *current = head;
+        // somePosition = n+1 (n is index)
+        while (current != nullptr){
+            current = current->next;
+        }
+
+
+
+
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
 
 // function that inserts given value to given position
 
@@ -12,4 +49,5 @@ using namespace std;
 
 // function that takes a position and returns that value without removing it
 
-// te
+// tests
+
--- linkedList.h
+++ linkedList.h
@@ -1,32 +1,5 @@
 #ifndef CS260_SPRING_23_ASSIGNMENT_4_LINKEDLIST_H
 #define CS260_SPRING_23_ASSIGNMENT_4_LINKEDLIST_H
 
-struct Node {
-    int data;
-    node* next;
-};
-
-
-class linkedList {
-private:
-    node* head;
-
-public:
-    linkedList() {
-        head = nullptr; // allows me to handle initial edge case
-    }
-
-    void add(int someValue, int somePosition){
-        node *new_node = new node;
-        new_node->data = someValue;
-        new_node->next = nullptr;
-
-        if (head == nullptr)
-
-
-
-    }
-};
-
-
 #endif
+
